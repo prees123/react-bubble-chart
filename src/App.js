28fff5852f7332ac7c8c68bb2c9efa06c8003048
Preejith Ponneth <preejithp@gmail.com>
2020-05-22 14:20:19 -0400

    Update readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BubbleChart from "@weknow/react-bubble-chart-d3";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BubbleChart
+          width={800}
+          height={800}
+          fontFamily="Arial"
+          data={[
+            { label: "CRM", value: 1 },
+            { label: "API", value: 1 },
+            { label: "Data", value: 1 },
+            { label: "Commerce", value: 1 },
+            { label: "AI", value: 3 },
+            { label: "Management", value: 5 },
+            { label: "Testing", value: 6 },
+            { label: "Mobile", value: 9 },
+            { label: "Conversion", value: 9 },
+            { label: "Misc", value: 21 },
+            { label: "Databases", value: 22 },
+            { label: "DevOps", value: 22 },
+            { label: "Javascript", value: 23 },
+            { label: "Languages / Frameworks", value: 25 },
+            { label: "Front End", value: 26 },
+            { label: "Content", value: 26 },
+          ]}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
